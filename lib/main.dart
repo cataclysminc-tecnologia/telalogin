50262afebfb76326bfe1eede8c0befade325cfe0
Cataclysm Inc. Tecnologia - MAC <cataclysminc.tecnologia@gmail.com>
2020-10-23 14:10:17 -0300

    Tela Login in Flutter - 23/10/2020 - 14:09

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:telalogin/pages/login.page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tela de Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
